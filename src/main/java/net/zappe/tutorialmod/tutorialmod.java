05ded61681f947bab47cf41bcc8446d63e4e881c
zapp-e <141183912+zapp-e@users.noreply.github.com>
2024-03-21 12:50:57 -0700

    custom items
    not done yet

--- src/main/java/net/zappe/tutorialmod/tutorialmod.java
+++ src/main/java/net/zappe/tutorialmod/tutorialmod.java
@@ -8,19 +8,22 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.zappe.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(tutorialmod.MOD_ID)
-public class tutorialmod
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod
 {
   
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public tutorialmod()
+    public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus)
 
         modEventBus.addListener(this::commonSetup);
 
